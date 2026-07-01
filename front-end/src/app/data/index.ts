a8e47767c65e34ec4a9d65f4fe67d36b60a32374
bima fahimna <bima.fahimna51@gmail.com>
2026-07-02 03:43:21 +0700

    feat: add diving video:

--- front-end/src/app/data/index.ts
+++ front-end/src/app/data/index.ts
@@ -106,7 +106,7 @@ export const hobbies: Hobby[] = [
 		description: 'Exploring the ocean’s depths and discovering marine life through scuba diving.',
 		assets: [
 			{ kind: 'image', src: '/hobby/diving/underwater_temple.JPG' },
-			{ kind: "video", src: '/hobby/diving/underboat.MP4' },
+			{ kind: "video", src: '/hobby/diving/underboat.mov' },
 			{ kind: 'image', src: '/hobby/diving/uw_selfie.png' },
 		],
 		span: 'wide',
